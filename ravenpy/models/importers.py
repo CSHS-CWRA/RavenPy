9b722ec99906bf912d6b1a6cacfe972f14830cb3
Christian Jauvin <cjauvin@gmail.com>
2020-12-13 22:11:42 -0500

    Grid weights importer WIP

--- ravenpy/models/importers.py
+++ ravenpy/models/importers.py
@@ -2,6 +2,8 @@ import geopandas
 import netCDF4 as nc4
 import numpy as np
 import xarray
+from osgeo import __version__ as osgeo_version
+from osgeo import ogr, osr
 
 from .commands import (
     ChannelProfileCommand,
@@ -203,29 +205,29 @@ class RoutingProductShapefileImporter:
 
 
 class GridWeights:
+
+    CRS_LLDEG = 4326  # EPSG id of lat/lon (deg) coordinate referenence system (CRS)
+    CRS_CAEA = 3573  # EPSG id of equal-area    coordinate referenence system (CRS)
+
     def __init__(self):
-        # nc_in = xarray.open_dataset(
-        #     "/home/christian/ouranos/raven/doc/Lievre/input/VIC_streaminputs.nc"
-        # )
         dimname = ["lon", "lat"]
         varname = ["lon", "lat"]
 
         routinginfo = "/home/christian/ouranos/raven/doc/Lievre/maps/LievreHRUs.shp"
 
-        crs_lldeg = 4326  # EPSG id of lat/lon (deg) coordinate referenence system (CRS)
-        crs_caea = 3573  # EPSG id of equal-area    coordinate referenence system (CRS)
-
         key_colname = "HRU_ID"
 
-        doall = False
+        doall = True
 
-        basin = "1802"
+        # basin = "OLE XX"
+        # basin = None
+        # SubId = ["7202"]
 
         nc_in = nc4.Dataset(
             "/home/christian/ouranos/raven/doc/Lievre/input/VIC_streaminputs.nc"
         )
 
-        # print(nc_in)
+        output_file = "/tmp/grid_weights.txt"
 
         lon = nc_in.variables[varname[0]][:]
         lon_dims = nc_in.variables[varname[0]].dimensions
@@ -244,44 +246,16 @@ class GridWeights:
         #
         if np.all(np.array(lon_dims) == dimname[::1]):
             lon = np.transpose(lon)
-            print(
-                '   >>> switched order of dimensions for variable "{0}"'.format(
-                    varname[0]
-                )
-            )
         elif np.all(np.array(lon_dims) == dimname[::-1]):
-            print(
-                '   >>> order of dimensions correct for variable "{0}"'.format(
-                    varname[0]
-                )
-            )
+            pass
         else:
-            print(
-                "   >>> Dimensions found {0} does not match the dimension names specified with (-d): {1}".format(
-                    lon_dims, dimname
-                )
-            )
             raise ValueError("STOP")
 
         if np.all(np.array(lat_dims) == dimname[::1]):
             lat = np.transpose(lat)
-            print(
-                '   >>> switched order of dimensions for variable "{0}"'.format(
-                    varname[1]
-                )
-            )
         elif np.all(np.array(lat_dims) == dimname[::-1]):
-            print(
-                '   >>> order of dimensions correct for variable "{0}"'.format(
-                    varname[1]
-                )
-            )
+            pass
         else:
-            print(
-                "   >>> Dimensions found {0} does not match the dimension names specified with (-d): {1}".format(
-                    lat_dims, dimname
-                )
-            )
             raise ValueError("STOP")
 
         lath, lonh = self.create_gridcells_from_centers(lat, lon)
@@ -292,129 +266,21 @@ class GridWeights:
         # -------------------------------
         # Read Basin shapes and all subbasin-shapes
         # -------------------------------
-        print(" ")
-        print("   (2) Reading routing toolbox data ...")
 
         shape = geopandas.read_file(routinginfo)
         # shape     = shape.to_crs(epsg=crs_lldeg)        # this is lat/lon in degree
-        shape = shape.to_crs(epsg=crs_caea)  # WGS 84 / North Pole LAEA Canada
+        # WGS 84 / North Pole LAEA Canada
+        shape = shape.to_crs(epsg=GridWeights.CRS_CAEA)
 
-        # check that key column contains only unique values
-        keys = np.array(list(shape[key_colname]))
-        # keys_uniq = np.unique(keys)
-        # if len(keys_uniq) != len(keys):
-        #     raise ValueError("The attribute of the shapefile set to contain only unique identifiers ('{}') does contain duplicate keys. Please specify another column (option -c '<col_name>') and use the option to process all records contained in the shapefile (-a).".format(key_colname))
-
-        # select only relevant basins/sub-basins
-        if not (doall):
-
-            if not (basin is None):  # if gauge ID is given
-
-                basins = [bb.strip() for bb in basin.split(",")]
-                idx_basins = [list(np.where(shape["Obs_NM"] == bb)[0]) for bb in basins]
-
-                # find corresponding SubId
-                SubId = [np.int(shape.loc[idx_basin].SubId) for idx_basin in idx_basins]
-                print("   >>> found gauge at SubId = ", SubId)
-
-            if not (SubId is None):  # if routing toolbox basin ID is given
-
-                old_SubIds = []
-                for SI in SubId:
-
-                    old_SubId = []
-                    new_SubId = [SI]
-
-                    while len(new_SubId) > 0:
-
-                        old_SubId.append(new_SubId)
-                        new_SubId = [
-                            list(
-                                shape.loc[(np.where(shape["DowSubId"] == ii))[0]].SubId
-                            )
-                            for ii in new_SubId
-                        ]  # find all upstream catchments of these new basins
-                        new_SubId = list(
-                            np.unique(
-                                [item for sublist in new_SubId for item in sublist]
-                            )
-                        )  # flatten list and make entries unique
-
-                    old_SubId = np.array(
-                        [item for sublist in old_SubId for item in sublist],
-                        dtype=np.int,
-                    )  # flatten list
-                    old_SubIds += list(old_SubId)
-
-                old_SubIds = list(np.sort(np.unique(old_SubIds)))
-
-                idx_basins = [
-                    list(np.where(shape["SubId"] == oo)[0]) for oo in old_SubIds
-                ]
-                idx_basins = [
-                    item for sublist in idx_basins for item in sublist
-                ]  # flatten list
-                idx_basins = list(
-                    np.unique(idx_basins)
-                )  # getting only unique list indexes
-
-        else:  # all HRUs to be processed
-
-            idx_basins = list(np.arange(0, len(shape)))
-
-        # make sure HRUs are only once in this list
-        hrus = np.array(shape.loc[idx_basins][key_colname])  # [sort_idx]
-
-        idx_basins_unique = []
-        hrus_unique = []
-        for ihru, hru in enumerate(hrus):
-
-            if not (hru in hrus_unique):
-
-                hrus_unique.append(hrus[ihru])
-                idx_basins_unique.append(idx_basins[ihru])
-
-        idx_basins = idx_basins_unique
-        hrus = hrus_unique
-
-        # order according to values in "key_colname"; just to make sure outputs will be sorted in the end
-        sort_idx = np.argsort(shape.loc[idx_basins][key_colname])
-        print(
-            "   >>> HRU_IDs found = ",
-            list(np.array(shape.loc[idx_basins][key_colname])[sort_idx]),
-            "  (total: ",
-            len(idx_basins),
-            ")",
-        )
-
-        # reduce the routing product dataset now to only what we will need
-        shape = shape.loc[np.array(idx_basins)[sort_idx]]
-
-        # indexes of all lines in df
-        keys = shape.index
-        nsubbasins = len(keys)
-
-        # initialize
-        coord_catch_wkt = {}
-
-        # loop over all subbasins and transform coordinates into equal-area projection
-        for kk in keys:
-
-            ibasin = shape.loc[kk]
-
-            poly = ibasin.geometry
-            coord_catch_wkt[kk] = ogr.CreateGeometryFromWkt(poly.to_wkt())
+        shape = shape.drop_duplicates(key_colname)
 
         # -------------------------------
         # construct all grid cell polygons
         # -------------------------------
-        print(" ")
-        print("   (3) Generate shapes for NetCDF grid cells ...")
 
         grid_cell_geom_gpd_wkt = [[[] for ilon in range(nlon)] for ilat in range(nlat)]
+
         for ilat in range(nlat):
-            if ilat % 10 == 0:
-                print("   >>> Latitudes done: {0} of {1}".format(ilat, nlat))
 
             for ilon in range(nlon):
 
@@ -461,99 +327,63 @@ class GridWeights:
                         [lath[ilat, ilon + 1], lonh[ilat, ilon + 1]],
                     ]
 
-                tmp = shape_to_geometry(gridcell_edges, epsg=crs_caea)
+                tmp = self.shape_to_geometry(gridcell_edges, epsg=GridWeights.CRS_CAEA)
                 grid_cell_geom_gpd_wkt[ilat][ilon] = tmp
 
         # -------------------------------
         # Derive overlay and calculate weights
         # -------------------------------
-        print(" ")
-        print("   (4) Deriving weights ...")
 
         filename = output_file
         ff = open(filename, "w")
         ff.write(":GridWeights                     \n")
         ff.write("   #                                \n")
         ff.write("   # [# HRUs]                       \n")
-        ff.write("   :NumberHRUs       {0}            \n".format(nsubbasins))
+        ff.write("   :NumberHRUs       {0}            \n".format(shape.shape[0]))
         ff.write("   :NumberGridCells  {0}            \n".format(nlon * nlat))
         ff.write("   #                                \n")
         ff.write("   # [HRU ID] [Cell #] [w_kl]       \n")
 
-        for ikk, kk in enumerate(keys):
+        for _, row in shape.iterrows():
 
-            ibasin = shape.loc[kk]
+            poly = ogr.CreateGeometryFromWkt(row.geometry.to_wkt())
 
-            area_basin = coord_catch_wkt[kk].Area()
-            enve_basin = coord_catch_wkt[
-                kk
-            ].GetEnvelope()  # bounding box around basin (for easy check of proximity)
-
-            area_all = 0.0
-            ncells = 0
+            area_basin = poly.Area()
+            # bounding box around basin (for easy check of proximity)
+            enve_basin = poly.GetEnvelope()
 
             for ilat in range(nlat):
                 for ilon in range(nlon):
 
-                    enve_gridcell = grid_cell_geom_gpd_wkt[ilat][
-                        ilon
-                    ].GetEnvelope()  # bounding box around grid-cell (for easy check of proximity)
-                    grid_is_close = check_proximity_of_envelops(
+                    # bounding box around grid-cell (for easy check of proximity)
+                    enve_gridcell = grid_cell_geom_gpd_wkt[ilat][ilon].GetEnvelope()
+                    grid_is_close = self.check_proximity_of_envelops(
                         enve_gridcell, enve_basin
                     )
 
-                    if (
-                        grid_is_close
-                    ):  # this check decreases runtime DRASTICALLY (from ~6h to ~1min)
+                    # this check decreases runtime DRASTICALLY (from ~6h to ~1min)
+                    if not grid_is_close:
+                        continue
 
-                        grid_cell_area = grid_cell_geom_gpd_wkt[ilat][ilon].Area()
+                    grid_cell_area = grid_cell_geom_gpd_wkt[ilat][ilon].Area()
 
-                        inter = grid_cell_geom_gpd_wkt[ilat][ilon].Intersection(
-                            coord_catch_wkt[kk].Buffer(0.0)
-                        )  # "fake" buffer to avoid invalid polygons and weirdos dumped by ArcGIS
-                        area_intersect = inter.Area()
+                    # "fake" buffer to avoid invalid polygons and weirdos dumped by ArcGIS
+                    inter = grid_cell_geom_gpd_wkt[ilat][ilon].Intersection(
+                        poly.Buffer(0.0)
+                    )
+                    area_intersect = inter.Area()
 
-                        area_all += area_intersect
-                        if area_intersect > 0:
-                            ncells += 1
-
-                            print(
-                                "   >>> {0},{1},{2},{3},{4}".format(
-                                    int(ibasin[key_colname]),
-                                    ilat,
-                                    ilon,
-                                    ilat * nlon + ilon,
-                                    area_intersect / area_basin,
-                                )
+                    if area_intersect > 0:
+                        ff.write(
+                            "   {0}   {1}   {2}\n".format(
+                                int(row[key_colname]),
+                                ilat * nlon + ilon,
+                                area_intersect / area_basin,
                             )
-                            ff.write(
-                                "   {0}   {1}   {2}\n".format(
-                                    int(ibasin[key_colname]),
-                                    ilat * nlon + ilon,
-                                    area_intersect / area_basin,
-                                )
-                            )
-
-            print(
-                "   >>> (Sub-)Basin: {0} ({1} of {2})".format(
-                    int(ibasin[key_colname]), ikk + 1, nsubbasins
-                )
-            )
-            print("   >>> Derived area of {0}  cells: {1}".format(ncells, area_all))
-            print("   >>> Read area from shapefile:   {0}".format(area_basin))
-            print(
-                "   >>> error:                      {0}%".format(
-                    (area_basin - area_all) / area_basin * 100.0
-                )
-            )
-            print("   ")
+                        )
 
         ff.write(":EndGridWeights \n")
         ff.close()
-
-        print("")
-        print("Wrote: ", filename)
-        print("")
 
     def create_gridcells_from_centers(self, lat, lon):
 
@@ -614,9 +444,9 @@ class GridWeights:
         poly_shape = ogr.Geometry(ogr.wkbPolygon)
         poly_shape.AddGeometry(ring_shape)
 
-        if not (epsg is None):
+        if epsg:
             source = osr.SpatialReference()
-            source.ImportFromEPSG(crs_lldeg)  # usual lat/lon projection
+            source.ImportFromEPSG(GridWeights.CRS_LLDEG)  # usual lat/lon projection
 
             target = osr.SpatialReference()
             target.ImportFromEPSG(epsg)  # any projection to convert to
@@ -635,20 +465,12 @@ class GridWeights:
         # minY  --> env[2]
         # maxY  --> env[3]
 
-        if (
-            (gridcell_envelop[0] <= shape_envelop[1])
-            and (gridcell_envelop[1] >= shape_envelop[0])
-            and (gridcell_envelop[2] <= shape_envelop[3])
-            and (gridcell_envelop[3] >= shape_envelop[2])
-        ):
-
-            grid_is_close = True
-
-        else:
-
-            grid_is_close = False
-
-        return grid_is_close
+        return (
+            gridcell_envelop[0] <= shape_envelop[1]
+            and gridcell_envelop[1] >= shape_envelop[0]
+            and gridcell_envelop[2] <= shape_envelop[3]
+            and gridcell_envelop[3] >= shape_envelop[2]
+        )
 
     def check_gridcell_in_proximity_of_shape(self, gridcell_edges, shape_from_jsonfile):
 
@@ -676,17 +498,9 @@ class GridWeights:
         min_lon_shape = np.min(lon_shape)
         max_lon_shape = np.max(lon_shape)
 
-        if (
-            (min_lat_cell <= max_lat_shape)
-            and (max_lat_cell >= min_lat_shape)
-            and (min_lon_cell <= max_lon_shape)
-            and (max_lon_cell >= min_lon_shape)
-        ):
-
-            grid_is_close = True
-
-        else:
-
-            grid_is_close = False
-
-        return grid_is_close
+        return (
+            min_lat_cell <= max_lat_shape
+            and max_lat_cell >= min_lat_shape
+            and min_lon_cell <= max_lon_shape
+            and max_lon_cell >= min_lon_shape
+        )
