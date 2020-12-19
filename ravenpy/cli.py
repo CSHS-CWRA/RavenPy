2dfa6bd84d651927cbde6fa4a273b5544e12b2f9
Christian Jauvin <cjauvin@gmail.com>
2020-12-19 11:43:23 -0500

    Support "legacy syntax" for ID lists for grid weidght script

--- ravenpy/cli.py
+++ ravenpy/cli.py
@@ -1,5 +1,6 @@
 """Console script for ravenpy."""
 import json
+import re
 import sys
 
 import click
@@ -51,7 +52,7 @@ def cli():
     "-s",
     "--sub-id",
     multiple=True,
-    type=int,
+    type=str,  # to support alternate "-s 123,456" legacy syntax
     show_default=True,
     help="Sub IDs of most downstream subbasins (containing usually a gauge station, corresponds to 'SubId' in the shapefile).",
 )
@@ -79,14 +80,28 @@ def generate_grid_weights(
 
     NC_FILE_PATH: NetCDF file containing at least 2D latitudes and 2D longitudes. Grid needs to be representative of model outputs that are then required to be routed.
     """
+
+    # Although Click does not support it, we want to support legacy syntax (e.g. "-s 123,456") for both gauge and sub IDs.
+    # The Click way for `multiple` options would be: "-s 123 -s 456"
+
+    if len(gauge_id) == 1:
+        gauge_ids = list(filter(None, re.split("\W+", gauge_id[0])))
+    else:
+        gauge_ids = gauge_id
+
+    if len(sub_id) == 1:
+        sub_ids = list(map(int, filter(None, re.split("\W+", sub_id[0]))))
+    else:
+        sub_ids = list(map(int, sub_id))
+
     importer = RoutingProductGridWeightImporter(
         shp_file_path,
         nc_file_path,
         dim_names,
         var_names,
         hru_id_field,
-        gauge_id,
-        sub_id,
+        gauge_ids,
+        sub_ids,
     )
     cmd = importer.extract()
     if output == "raven":
