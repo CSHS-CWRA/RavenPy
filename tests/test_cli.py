d0fba0dedd27a8e03f2e315b6116c16897eb03c5
Christian Jauvin <cjauvin@gmail.com>
2020-12-23 12:35:47 -0500

    Simplify weight verification logic in test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,10 @@
+import re
+
 from click.testing import CliRunner
 
 from ravenpy.cli import generate_grid_weights
 
 from .common import get_test_data
-
-import numpy as np
 
 
 def test_generate_grid_weights_with_nc_input_and_2d_coords():
@@ -20,17 +20,12 @@ def test_generate_grid_weights_with_nc_input_and_2d_coords():
     assert result.exit_code == 0
     assert not result.exception
     assert ":NumberHRUs 51" in result.output
-    assert ":NumberGridCells 100" in result.output   
+    assert ":NumberGridCells 100" in result.output
     assert len(result.output.split("\n")) == 216
 
     # check derived weights
-    target_HRU_ID = 1
-    target_cell_ID = 52
-    target_weight = 0.2610203097218425
-    result_as_array = result.output.split("\n")[3:-2]  # remove header and footer lines 
-    result_as_array = np.array( [  list(map(float,ii.strip().split())) for ii in result_as_array ] ) # convert to array
-    weight = result_as_array[np.logical_and(result_as_array[:,0]==target_HRU_ID, result_as_array[:,1]==target_cell_ID),2][0] # find row with predefined HRU_ID and Cell_ID and extract weight
-    assert np.isclose(weight, target_weight, rtol=1e-04, atol=1e-04, equal_nan=False)
+    weight = float(re.search("1 52 (.+)", result.output).group(1))
+    assert abs(weight - 0.2610203097218425) < 1e-04
 
 
 def test_generate_grid_weights_with_multiple_subids():
@@ -50,17 +45,12 @@ def test_generate_grid_weights_with_multiple_subids():
     assert result.exit_code == 0
     assert not result.exception
     assert ":NumberHRUs 51" in result.output
-    assert ":NumberGridCells 100" in result.output    
+    assert ":NumberGridCells 100" in result.output
     assert len(result.output.split("\n")) == 216
 
     # check derived weights
-    target_HRU_ID = 1
-    target_cell_ID = 52
-    target_weight = 0.2610203097218425
-    result_as_array = result.output.split("\n")[3:-2]  # remove header and footer lines 
-    result_as_array = np.array( [  list(map(float,ii.strip().split())) for ii in result_as_array ] ) # convert to array
-    weight = result_as_array[np.logical_and(result_as_array[:,0]==target_HRU_ID, result_as_array[:,1]==target_cell_ID),2][0] # find row with predefined HRU_ID and Cell_ID and extract weight
-    assert np.isclose(weight, target_weight, rtol=1e-04, atol=1e-04, equal_nan=False)
+    weight = float(re.search("1 52 (.+)", result.output).group(1))
+    assert abs(weight - 0.2610203097218425) < 1e-04
 
 
 def test_generate_grid_weights_with_nc_input_and_1d_coords():
@@ -83,13 +73,8 @@ def test_generate_grid_weights_with_nc_input_and_1d_coords():
     assert len(result.output.split("\n")) == 128
 
     # check derived weights
-    target_HRU_ID = 4
-    target_cell_ID = 3731
-    target_weight = 0.0034512752779023515
-    result_as_array = result.output.split("\n")[3:-2]  # remove header and footer lines 
-    result_as_array = np.array( [  list(map(float,ii.strip().split())) for ii in result_as_array ] ) # convert to array
-    weight = result_as_array[np.logical_and(result_as_array[:,0]==target_HRU_ID, result_as_array[:,1]==target_cell_ID),2][0] # find row with predefined HRU_ID and Cell_ID and extract weight
-    assert np.isclose(weight, target_weight, rtol=1e-04, atol=1e-04, equal_nan=False)
+    weight = float(re.search("4 3731 (.+)", result.output).group(1))
+    assert abs(weight - 0.0034512752779023515) < 1e-04
 
 
 def test_generate_grid_weights_with_shp_input():
@@ -109,13 +94,8 @@ def test_generate_grid_weights_with_shp_input():
     assert len(result.output.split("\n")) == 230
 
     # check derived weights
-    target_HRU_ID = 13
-    target_cell_ID = 238
-    target_weight = 0.5761414847779369
-    result_as_array = result.output.split("\n")[3:-2]  # remove header and footer lines 
-    result_as_array = np.array( [  list(map(float,ii.strip().split())) for ii in result_as_array ] ) # convert to array
-    weight = result_as_array[np.logical_and(result_as_array[:,0]==target_HRU_ID, result_as_array[:,1]==target_cell_ID),2][0] # find row with predefined HRU_ID and Cell_ID and extract weight
-    assert np.isclose(weight, target_weight, rtol=1e-04, atol=1e-04, equal_nan=False)
+    weight = float(re.search("13 238 (.+)", result.output).group(1))
+    assert abs(weight - 0.5761414847779369) < 1e-04
 
 
 def test_generate_grid_weights_with_weight_rescaling():
@@ -137,10 +117,5 @@ def test_generate_grid_weights_with_weight_rescaling():
     assert len(result.output.split("\n")) == 230
 
     # check derived weights
-    target_HRU_ID = 13
-    target_cell_ID = 238
-    target_weight = 0.9851111335377887
-    result_as_array = result.output.split("\n")[3:-2]  # remove header and footer lines 
-    result_as_array = np.array( [  list(map(float,ii.strip().split())) for ii in result_as_array ] ) # convert to array
-    weight = result_as_array[np.logical_and(result_as_array[:,0]==target_HRU_ID, result_as_array[:,1]==target_cell_ID),2][0] # find row with predefined HRU_ID and Cell_ID and extract weight
-    assert np.isclose(weight, target_weight, rtol=1e-04, atol=1e-04, equal_nan=False)
+    weight = float(re.search("13 238 (.+)", result.output).group(1))
+    assert abs(weight - 0.9851111335377887) < 1e-04
