c23f757854415f4a5bca7027a82bc9d17e5fd729
Julie <juliane.mai@uwaterloo.ca>
2021-02-10 13:59:57 -0500

    Minor cleanup

--- ravenpy/models/aggregate_forcings_to_HRUs.py
+++ ravenpy/models/aggregate_forcings_to_HRUs.py
@@ -1,46 +1,14 @@
-#!/usr/bin/env python
-from __future__ import print_function
-
-# Copyright 2016-2018 Juliane Mai - juliane.mai(at)uwaterloo.ca
-#
-# License
-# This file is part of Juliane Mai's personal code library.
-#
-# Juliane Mai's personal code library is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Juliane Mai's personal code library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License
-# along with Juliane Mai's personal code library.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-
 from ravenpy.models.importers import (
-    RoutingProductGridWeightImporter,
-    RoutingProductShapefileImporter,    
+    RoutingProductGridWeightImporter,  
 )
-from ravenpy.models.commands import GriddedForcingCommand
-#from ravenpy.utilities.testdata import get_local_testdata
-
 import netCDF4 as nc4
 
 
-
-
-CRS_LLDEG = 4326  # EPSG id of lat/lon (deg) coordinate referenence system (CRS)
-CRS_CAEA = 3573  # EPSG id of equal-area    coordinate referenence system (CRS)
-ROUTING_ID_FIELD = "HRU_ID"
-NETCDF_INPUT_FIELD = "NetCDF_col"
 DIM_NAMES = ("lon", "lat")
 VAR_NAMES = ("lon", "lat")
+ROUTING_ID_FIELD = "HRU_ID"
+NETCDF_INPUT_FIELD = "NetCDF_col"
 AREA_ERROR_THRESHOLD = 0.05
-
 VARIABLES_TO_AGGREGATE = ["Streaminputs"]
 
 def aggregate_forcings_to_HRUs( input_file, routing_file,
@@ -88,19 +56,17 @@ def aggregate_forcings_to_HRUs( input_file, routing_file,
 
 
 
+# from ravenpy.utilities.testdata import get_local_testdata
 
+# input_file = get_local_testdata("raven-routing-sample/VIC_streaminputs.nc")
+# routing_file = get_local_testdata("raven-routing-sample/finalcat_hru_info.zip")
 
-# switched to local paths because with "get_local_testdata" following error:
-#     RuntimeError: RAVENPY_TESTDATA_PATH env variable is not set
-input_file = "/Users/j6mai/Documents/GitHub/GridWeightsGenerator/example/input_VIC/VIC_streaminputs.nc"
-routing_file = "/Users/j6mai/Documents/GitHub/GridWeightsGenerator/example/maps/finalcat_hru_info.zip"
-
-aggregate_forcings_to_HRUs( input_file, routing_file,
-                                    dim_names=DIM_NAMES,
-                                    var_names=VAR_NAMES,
-                                    routing_id_field=ROUTING_ID_FIELD,
-                                    netcdf_input_field=NETCDF_INPUT_FIELD,
-                                    gauge_ids=None,
-                                    sub_ids=None,
-                                    area_error_threshold=AREA_ERROR_THRESHOLD,
-                                    variables_to_aggregate=VARIABLES_TO_AGGREGATE )
+# aggregate_forcings_to_HRUs( input_file, routing_file,
+#                                     dim_names=DIM_NAMES,
+#                                     var_names=VAR_NAMES,
+#                                     routing_id_field=ROUTING_ID_FIELD,
+#                                     netcdf_input_field=NETCDF_INPUT_FIELD,
+#                                     gauge_ids=None,
+#                                     sub_ids=None,
+#                                     area_error_threshold=AREA_ERROR_THRESHOLD,
+#                                     variables_to_aggregate=VARIABLES_TO_AGGREGATE )
