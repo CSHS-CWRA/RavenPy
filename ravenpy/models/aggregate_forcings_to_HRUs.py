e68ed1c2e97b1b0bc67c51f94dc960a12ce9a03f
Christian Jauvin <cjauvin@gmail.com>
2021-02-10 19:09:20 -0500

    Remove the big NC file

--- ravenpy/models/aggregate_forcings_to_HRUs.py
+++ ravenpy/models/aggregate_forcings_to_HRUs.py
@@ -2,6 +2,7 @@ from ravenpy.models.importers import (
     RoutingProductGridWeightImporter,  
 )
 import netCDF4 as nc4
+import numpy as np
 
 
 DIM_NAMES = ("lon", "lat")
@@ -11,7 +12,7 @@ NETCDF_INPUT_FIELD = "NetCDF_col"
 AREA_ERROR_THRESHOLD = 0.05
 VARIABLES_TO_AGGREGATE = ["Streaminputs"]
 
-def aggregate_forcings_to_HRUs( input_file, routing_file,
+def aggregate_forcings_to_HRUs( input_file, routing_file, output_file,
                                     dim_names=DIM_NAMES,
                                     var_names=VAR_NAMES,
                                     routing_id_field=ROUTING_ID_FIELD,
@@ -19,7 +20,8 @@ def aggregate_forcings_to_HRUs( input_file, routing_file,
                                     gauge_ids=None,
                                     sub_ids=None,
                                     area_error_threshold=AREA_ERROR_THRESHOLD,
-                                    variables_to_aggregate=VARIABLES_TO_AGGREGATE ):
+                                    variables_to_aggregate=VARIABLES_TO_AGGREGATE,
+                                    ):
 
     """
     arguments:
@@ -41,32 +43,134 @@ def aggregate_forcings_to_HRUs( input_file, routing_file,
     nHRU         = gws.number_hrus
     nCells       = gws.number_grid_cells
     weights_data = gws.data
-    
-    # cell_id = ilat * self._nlon + ilon
-    print(weights_data[20])
-    
-    
+
+
     # read NetCDF
-    input_data = nc4.Dataset(input_file)
-    
+    nc_in = nc4.Dataset(input_file,'r')
+
+    # length of dimensions
+    nlon  = nc_in.dimensions[dim_names[0]].size
+    nlat  = nc_in.dimensions[dim_names[1]].size
+    ntime = nc_in.dimensions['time'].size
+
+    # check that this matches the nCells from grid-weights
+    # (basically making sure these are the same NetCDF files; not sure how that would happen but just to be on the safe side)
+    if nlon*nlat != nCells:
+        raise ValueError(
+                "Number of cells used to derive grid-weights does not match NetCDF provided for aggregating variables"
+            )
+
+    # convert cell_id (from weights) into (lat_id and lon_id)
+    # cell_id = ilat * nlon + ilon
+    # ---> ilon = cell_id %  nlon
+    # ---> ilat = cell_id // nlon
+    weights_data_lon_lat_ids = []
+    for iweights_data in weights_data:
+
+        cell_id = iweights_data[1]
+        ilon = cell_id %  nlon
+        ilat = cell_id // nlon
+        weights_data_lon_lat_ids.append( tuple( [ iweights_data[0],ilon,ilat,iweights_data[2] ] ) )
+    weights_data_lon_lat_ids = np.array(weights_data_lon_lat_ids)   # because I dont know how to do most operations elegantly with lists of tuples
+
+    # create new NetCDF that will contain aggregated data of listed variables
+    nc_out = nc4.Dataset(output_file,'w')
+    nc_dim_time = nc_out.createDimension('time',ntime)
+    nc_dim_hrus = nc_out.createDimension('nHRU',nHRU)
+
+    # create all variables in output NC (incl. time) and copy over all attributes
+    for name, variable in nc_in.variables.items():
+        if name in variables_to_aggregate+['time']:
+
+            if name != 'time':
+                dims = ['time','nHRU']
+            else:
+                dims = ['time']
+            x = nc_out.createVariable(name, variable.datatype, dims, zlib=True)
+
+            # copy variable attributes all at once via dictionary
+            nc_out[name].setncatts(nc_in[name].__dict__)
+
+            # copy over time variable data
+            if name == 'time':
+                nc_out[name][:] = nc_in[name][:]
+
+
     for variable_to_aggregate in variables_to_aggregate:
-        input_var  = input_data.variables[variable_to_aggregate]
 
-    return nHRU    # dummy return for now :)
+        # read 3D variable
+        input_var  = nc_in.variables[variable_to_aggregate]
+
+        # read 2D variable (to be able to write to it later)
+        output_var  = nc_out.variables[variable_to_aggregate]
+
+        # is variable 3D ?
+        ndims = len(input_var.dimensions)
+        if ndims != 3:
+            raise ValueError(
+                    "Input variable to aggregate needs to be 3D"
+                )
+        
+        # what is the order of dimensions?
+        idx_lon_dim  = input_var.dimensions.index( dim_names[0] )
+        idx_lat_dim  = input_var.dimensions.index( dim_names[1] )
+        idx_time_dim = input_var.dimensions.index('time')
+
+        # list of HRU IDs
+        hrus = np.unique( weights_data_lon_lat_ids[:,0] )
+
+        if len(hrus) != nHRU:
+            # should really never happen
+            raise ValueError(
+                "Number of weights found in grid weights list is not matching the number indicated there by nHRUs"
+                )
+
+        # do actual aggregation
+        agg_var = np.zeros( [ntime,nHRU] )
+        new_weights = []
+        for ihru,hru in enumerate(hrus):
+
+            hru=int(hru)
+
+            # filter all weights for current HRU
+            idx = np.where( weights_data_lon_lat_ids[:,0] == hru )[0]
+
+            for ii in idx:
+                # bring idx for input_var in appropriate order
+                idx_input = [slice(0,ntime,1), slice(0,ntime,1), slice(0,ntime,1)]
+                idx_input[idx_lon_dim] = int(weights_data_lon_lat_ids[ii,1])
+                idx_input[idx_lat_dim] = int(weights_data_lon_lat_ids[ii,2])
+                agg_var[:,ihru] +=  input_var[idx_input] * weights_data_lon_lat_ids[ii,3]
+
+            # create new weights: now each HRU is exactly one "grid-cell"
+            new_weights.append( tuple( [hru,ihru,1.0] ) )
+
+        # write 2D variable
+        output_var[:] = agg_var
+            
+    nc_out.close()
+
+    # the return should actually look exactly like the return of "RoutingProductGridWeightImporter"
+    # not sure how to do that
+
+    return new_weights
 
 
 
-# from ravenpy.utilities.testdata import get_local_testdata
 
-# input_file = get_local_testdata("raven-routing-sample/VIC_streaminputs.nc")
-# routing_file = get_local_testdata("raven-routing-sample/finalcat_hru_info.zip")
 
-# aggregate_forcings_to_HRUs( input_file, routing_file,
-#                                     dim_names=DIM_NAMES,
-#                                     var_names=VAR_NAMES,
-#                                     routing_id_field=ROUTING_ID_FIELD,
-#                                     netcdf_input_field=NETCDF_INPUT_FIELD,
-#                                     gauge_ids=None,
-#                                     sub_ids=None,
-#                                     area_error_threshold=AREA_ERROR_THRESHOLD,
-#                                     variables_to_aggregate=VARIABLES_TO_AGGREGATE )
+# input_file = "/Users/j6mai/Documents/GitHub/GridWeightsGenerator/example/input_VIC/VIC_streaminputs.nc"	
+# routing_file = "/Users/j6mai/Documents/GitHub/GridWeightsGenerator/example/maps/finalcat_hru_info.zip"
+# output_file = "/Users/j6mai/Documents/GitHub/GridWeightsGenerator/example/input_VIC/VIC_streaminputs_agg.nc"	
+# variables_to_aggregate=VARIABLES_TO_AGGREGATE
+# dim_names=DIM_NAMES
+# var_names=VAR_NAMES
+# routing_id_field=ROUTING_ID_FIELD
+# netcdf_input_field=NETCDF_INPUT_FIELD
+# gauge_ids=None
+# sub_ids=None
+# area_error_threshold=AREA_ERROR_THRESHOLD
+
+
+
+
--- tests/test_aggregate_forcings_to_HRUs.py
+++ tests/test_aggregate_forcings_to_HRUs.py
@@ -1,14 +1,22 @@
 import numpy as np
+import netCDF4 as nc4
 import pytest
 
 from ravenpy.models.aggregate_forcings_to_HRUs import aggregate_forcings_to_HRUs
 from ravenpy.utilities.testdata import get_local_testdata
 
 
+# run test:
+#   rpy
+#   cd /Users/j6mai/Documents/GitHub/RavenPy/
+#   pytest tests/test_aggregate_forcings_to_HRUs.py 
+
+
 def test_aggregate_forcings_to_HRUs():
 
     input_file = get_local_testdata("raven-routing-sample/VIC_streaminputs.nc")
     routing_file = get_local_testdata("raven-routing-sample/finalcat_hru_info.zip")
+    output_file = "/tmp/VIC_streaminputs_aggregated.nc"   # this should be something better
 
     DIM_NAMES = ("lon", "lat")
     VAR_NAMES = ("lon", "lat")
@@ -17,7 +25,7 @@ def test_aggregate_forcings_to_HRUs():
     AREA_ERROR_THRESHOLD = 0.05
     VARIABLES_TO_AGGREGATE = ["Streaminputs"]
 
-    agg_out = aggregate_forcings_to_HRUs( input_file, routing_file,
+    new_weights = aggregate_forcings_to_HRUs( input_file, routing_file, output_file,
                                     dim_names=DIM_NAMES,
                                     var_names=VAR_NAMES,
                                     routing_id_field=ROUTING_ID_FIELD,
@@ -27,6 +35,25 @@ def test_aggregate_forcings_to_HRUs():
                                     area_error_threshold=AREA_ERROR_THRESHOLD,
                                     variables_to_aggregate=VARIABLES_TO_AGGREGATE )
 
+    # check new weights
+    assert new_weights[0][0] == 1            # These are the HRU-IDs
+    
+    assert new_weights[0][1] == 0            # These need to be exactly [0,1,2,3,...,nHRU]
+    assert new_weights[1][1] == 1            # These need to be exactly [0,1,2,3,...,nHRU]
+    assert new_weights[2][1] == 2            # These need to be exactly [0,1,2,3,...,nHRU]
+    assert new_weights[3][1] == 3            # These need to be exactly [0,1,2,3,...,nHRU]
+    
+    assert new_weights[0][2] == 1.0          # All new_weights[:][2] need to be 1.0
+    assert new_weights[1][2] == 1.0          # All new_weights[:][2] need to be 1.0
+    assert new_weights[2][2] == 1.0          # All new_weights[:][2] need to be 1.0
+    assert new_weights[3][2] == 1.0          # All new_weights[:][2] need to be 1.0
 
-    assert agg_out == 51   # dummy result for now :)
 
+    # check aggregated NetCDF file 
+    nc_in = nc4.Dataset(output_file,'r')
+    val = nc_in.variables[VARIABLES_TO_AGGREGATE[0]][:]
+    nc_in.close()
+    assert abs(val[    0, 0] - 0.017309) < 1e-04
+    assert abs(val[16071, 0] - 0.569977) < 1e-04
+    assert abs(val[    0,50] - 0.010276) < 1e-04
+    assert abs(val[16071,50] - 0.516639) < 1e-04
