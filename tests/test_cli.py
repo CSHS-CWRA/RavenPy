b12c3f55caad0cd71926cc3349c869e49591f86a
Christian Jauvin <cjauvin@gmail.com>
2021-02-27 11:08:06 -0500

    Add TestGenerateGridWeights class in test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,147 +7,144 @@ from ravenpy.cli import generate_grid_weights
 from ravenpy.utilities.testdata import get_local_testdata
 
 
-def test_generate_grid_weights_with_nc_input_and_2d_coords(tmp_path):
-    runner = CliRunner()
-    output_path = tmp_path / "bla.rvt"
-    params = [
-        get_local_testdata("raven-routing-sample/VIC_streaminputs.nc"),
-        get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
-        "-o",
-        output_path,
-    ]
-    params = map(str, params)
+class TestGenerateGridWeights:
+    def test_generate_grid_weights_with_nc_input_and_2d_coords(self, tmp_path):
+        runner = CliRunner()
+        output_path = tmp_path / "bla.rvt"
+        params = [
+            get_local_testdata("raven-routing-sample/VIC_streaminputs.nc"),
+            get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
+            "-o",
+            output_path,
+        ]
+        params = map(str, params)
 
-    result = runner.invoke(generate_grid_weights, params)
+        result = runner.invoke(generate_grid_weights, params)
 
-    assert result.exit_code == 0
-    assert not result.exception
+        assert result.exit_code == 0
+        assert not result.exception
 
-    output = output_path.read_text()
+        output = output_path.read_text()
 
-    assert ":NumberHRUs 51" in output
-    assert ":NumberGridCells 100" in output
-    assert len(output.split("\n")) == 216
+        assert ":NumberHRUs 51" in output
+        assert ":NumberGridCells 100" in output
+        assert len(output.split("\n")) == 216
 
-    # check derived weights
-    weight = float(re.search("1 52 (.+)", output).group(1))
-    assert abs(weight - 0.2610203097218425) < 1e-04
+        # check derived weights
+        weight = float(re.search("1 52 (.+)", output).group(1))
+        assert abs(weight - 0.2610203097218425) < 1e-04
 
+    def test_generate_grid_weights_with_multiple_subids(self, tmp_path):
+        # currently exactly same output as "test_generate_grid_weights_with_nc_input_and_2d_coords"
+        # needs a "routing-file-path" with multiple gauges
+        runner = CliRunner()
+        output_path = tmp_path / "bla.rvt"
+        params = [
+            get_local_testdata("raven-routing-sample/VIC_streaminputs.nc"),
+            get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
+            "-s",
+            "7202",
+            "-s",
+            "6248",
+            "-o",
+            output_path,
+        ]
+        params = map(str, params)
 
-def test_generate_grid_weights_with_multiple_subids(tmp_path):
-    # currently exactly same output as "test_generate_grid_weights_with_nc_input_and_2d_coords"
-    # needs a "routing-file-path" with multiple gauges
-    runner = CliRunner()
-    output_path = tmp_path / "bla.rvt"
-    params = [
-        get_local_testdata("raven-routing-sample/VIC_streaminputs.nc"),
-        get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
-        "-s",
-        "7202",
-        "-s",
-        "6248",
-        "-o",
-        output_path,
-    ]
-    params = map(str, params)
+        result = runner.invoke(generate_grid_weights, params)
 
-    result = runner.invoke(generate_grid_weights, params)
+        assert result.exit_code == 0
+        assert not result.exception
 
-    assert result.exit_code == 0
-    assert not result.exception
+        output = output_path.read_text()
 
-    output = output_path.read_text()
+        assert ":NumberHRUs 51" in output
+        assert ":NumberGridCells 100" in output
+        assert len(output.split("\n")) == 216
 
-    assert ":NumberHRUs 51" in output
-    assert ":NumberGridCells 100" in output
-    assert len(output.split("\n")) == 216
+        # check derived weights
+        weight = float(re.search("1 52 (.+)", output).group(1))
+        assert abs(weight - 0.2610203097218425) < 1e-04
 
-    # check derived weights
-    weight = float(re.search("1 52 (.+)", output).group(1))
-    assert abs(weight - 0.2610203097218425) < 1e-04
+    def test_generate_grid_weights_with_nc_input_and_1d_coords(self, tmp_path):
+        runner = CliRunner()
+        output_path = tmp_path / "bla.rvt"
+        params = [
+            get_local_testdata("raven-routing-sample/era5-test-dataset-crop.nc"),
+            get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
+            "--var-names",
+            "longitude",
+            "latitude",
+            "-o",
+            output_path,
+        ]
+        params = map(str, params)
 
+        result = runner.invoke(generate_grid_weights, params)
 
-def test_generate_grid_weights_with_nc_input_and_1d_coords(tmp_path):
-    runner = CliRunner()
-    output_path = tmp_path / "bla.rvt"
-    params = [
-        get_local_testdata("raven-routing-sample/era5-test-dataset-crop.nc"),
-        get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
-        "--var-names",
-        "longitude",
-        "latitude",
-        "-o",
-        output_path,
-    ]
-    params = map(str, params)
+        assert result.exit_code == 0
+        assert not result.exception
 
-    result = runner.invoke(generate_grid_weights, params)
+        output = output_path.read_text()
 
-    assert result.exit_code == 0
-    assert not result.exception
+        assert ":NumberHRUs 51" in output
+        assert ":NumberGridCells 9801" in output
+        assert len(output.split("\n")) == 128
 
-    output = output_path.read_text()
+        # check derived weights
+        weight = float(re.search("4 3731 (.+)", output).group(1))
+        assert abs(weight - 0.0034512752779023515) < 1e-04
 
-    assert ":NumberHRUs 51" in output
-    assert ":NumberGridCells 9801" in output
-    assert len(output.split("\n")) == 128
+    def test_generate_grid_weights_with_shp_input(self, tmp_path):
+        runner = CliRunner()
+        output_path = tmp_path / "bla.rvt"
+        params = [
+            get_local_testdata("raven-routing-sample/OTT_sub.zip"),
+            get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
+            "-o",
+            output_path,
+        ]
+        params = map(str, params)
 
-    # check derived weights
-    weight = float(re.search("4 3731 (.+)", output).group(1))
-    assert abs(weight - 0.0034512752779023515) < 1e-04
+        result = runner.invoke(generate_grid_weights, params)
 
+        assert result.exit_code == 0
+        assert not result.exception
 
-def test_generate_grid_weights_with_shp_input(tmp_path):
-    runner = CliRunner()
-    output_path = tmp_path / "bla.rvt"
-    params = [
-        get_local_testdata("raven-routing-sample/OTT_sub.zip"),
-        get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
-        "-o",
-        output_path,
-    ]
-    params = map(str, params)
+        output = output_path.read_text()
 
-    result = runner.invoke(generate_grid_weights, params)
+        assert ":NumberHRUs 51" in output
+        assert ":NumberGridCells 810" in output
+        assert len(output.split("\n")) == 230
 
-    assert result.exit_code == 0
-    assert not result.exception
+        # check derived weights
+        weight = float(re.search("13 238 (.+)", output).group(1))
+        assert abs(weight - 0.5761414847779369) < 1e-04
 
-    output = output_path.read_text()
+    def test_generate_grid_weights_with_weight_rescaling(self, tmp_path):
+        runner = CliRunner()
+        output_path = tmp_path / "bla.rvt"
+        params = [
+            get_local_testdata("raven-routing-sample/OTT_sub.zip"),
+            get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
+            "--area-error-threshold",
+            "0.42",
+            "-o",
+            output_path,
+        ]
+        params = map(str, params)
 
-    assert ":NumberHRUs 51" in output
-    assert ":NumberGridCells 810" in output
-    assert len(output.split("\n")) == 230
+        result = runner.invoke(generate_grid_weights, params)
 
-    # check derived weights
-    weight = float(re.search("13 238 (.+)", output).group(1))
-    assert abs(weight - 0.5761414847779369) < 1e-04
+        assert result.exit_code == 0
+        assert not result.exception
 
+        output = output_path.read_text()
 
-def test_generate_grid_weights_with_weight_rescaling(tmp_path):
-    runner = CliRunner()
-    output_path = tmp_path / "bla.rvt"
-    params = [
-        get_local_testdata("raven-routing-sample/OTT_sub.zip"),
-        get_local_testdata("raven-routing-sample/finalcat_hru_info.zip"),
-        "--area-error-threshold",
-        "0.42",
-        "-o",
-        output_path,
-    ]
-    params = map(str, params)
+        assert ":NumberHRUs 51" in output
+        assert ":NumberGridCells 810" in output
+        assert len(output.split("\n")) == 230
 
-    result = runner.invoke(generate_grid_weights, params)
-
-    assert result.exit_code == 0
-    assert not result.exception
-
-    output = output_path.read_text()
-
-    assert ":NumberHRUs 51" in output
-    assert ":NumberGridCells 810" in output
-    assert len(output.split("\n")) == 230
-
-    # check derived weights
-    weight = float(re.search("13 238 (.+)", output).group(1))
-    assert abs(weight - 0.9851111335377887) < 1e-04
+        # check derived weights
+        weight = float(re.search("13 238 (.+)", output).group(1))
+        assert abs(weight - 0.9851111335377887) < 1e-04
