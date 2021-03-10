8f31afd864dd6de58f2e419cfc45bf27b32737aa
Julie <juliane.mai@uwaterloo.ca>
2021-03-09 21:22:16 -0500

    Adding new model BLENDED to model suite
    BLENDED model allows for calibration of model parameters and model structure

---
+++ ravenpy/models/ostrich-blended/raven-blended.rvp.tpl
@@ -0,0 +1,128 @@
+#########################################################################
+:FileType          rvp ASCII Raven 3.0.4
+:WrittenBy         Juliane Mai & James Craig
+:CreationDate      Feb 2021
+#
+# Emulation of Blended model simulation of Salmon River near Prince George
+#------------------------------------------------------------------------
+#
+
+# tied parameters:
+# (it is important for OSTRICH to find every parameter place holder somewhere in this file)
+# (without this "par_x25" and "par_x14" and "par_x10" wouldn't be detectable)
+#    para_sum_x24_x25 = sum_x24_x25 =  para_x24 + para_x25 = par_x24 + par_x25
+#    para_sum_x13_x14 = sum_x13_x14 =  para_x13 + para_x14 = par_x13 + par_x14
+#    para_sum_x09_x10 = sum_x09_x10 =  para_x09 + para_x10 = par_x09 + par_x10
+#    para_pow_x04     = pow_x04     =  10^(para_x04)       = 10^par_x04
+#    para_pow_x11     = pow_x11     =  10^(para_x11)       = 10^par_x11
+
+# RVT shopuld contain
+#  :RainCorrection par_x33
+#  :SnowCorrection par_x34
+
+#-----------------------------------------------------------------
+# Soil Classes
+#-----------------------------------------------------------------
+:SoilClasses
+  :Attributes,
+  :Units,
+  TOPSOIL,
+  PHREATIC,
+  DEEP_GW
+:EndSoilClasses
+
+#-----------------------------------------------------------------
+# Land Use Classes
+#-----------------------------------------------------------------
+:LandUseClasses,
+  :Attributes,        IMPERM,    FOREST_COV,
+       :Units,          frac,          frac,
+       FOREST,           0.0,       0.02345,
+:EndLandUseClasses
+
+#-----------------------------------------------------------------
+# Vegetation Classes
+#-----------------------------------------------------------------
+:VegetationClasses,
+  :Attributes,        MAX_HT,       MAX_LAI, MAX_LEAF_COND,
+       :Units,             m,          none,      mm_per_s,
+       FOREST,             4,             5,             5,
+:EndVegetationClasses
+
+#-----------------------------------------------------------------
+# Soil Profiles
+#-----------------------------------------------------------------
+:SoilProfiles
+         LAKE, 0
+         ROCK, 0
+  DEFAULT_P, 3, TOPSOIL,    par_x29, PHREATIC,    par_x30, DEEP_GW, 1e6
+# DEFAULT_P, 3, TOPSOIL,      x(29), PHREATIC,      x(30), DEEP_GW, 1e6
+:EndSoilProfiles
+
+#-----------------------------------------------------------------
+# Terrain Classes
+#-----------------------------------------------------------------
+:TerrainClasses
+  :Attributes,        hillslope_len, drainage_dens,            lambda,
+       :Units,                   ??,            ??,                ??
+    DEFAULT_T,                  1.0,           1.0,           par_x07
+#                                                     TOPMODEL_LAMBDA x(7)
+:EndTerrainClasses
+
+#-----------------------------------------------------------------
+# Global Parameters
+#-----------------------------------------------------------------
+:GlobalParameter         SNOW_SWI_MIN par_x13            # x(13)
+:GlobalParameter         SNOW_SWI_MAX sum_x13_x14   # x(13)+x(14)
+:GlobalParameter     SWI_REDUCT_COEFF par_x15            # x(15)
+:GlobalParameter             SNOW_SWI par_x19            # x(19)
+:GlobalParameter        RAINSNOW_TEMP par_x31            # x(31)
+:GlobalParameter       RAINSNOW_DELTA par_x32            # x(32)
+#:GlobalParameter      TOC_MULTIPLIER 1.0                   #
+
+
+#-----------------------------------------------------------------
+# Soil Parameters
+#-----------------------------------------------------------------
+:SoilParameterList
+  :Parameters,        POROSITY,      PERC_COEFF,  PET_CORRECTION,  BASEFLOW_COEFF,      B_EXP,   HBV_BETA, MAX_BASEFLOW_RATE, BASEFLOW_N,      FIELD_CAPACITY,   SAT_WILT,
+       :Units,               -,             1/d,               -,             1/d
+      TOPSOIL,             1.0,         par_x28,         par_x08,         pow_x04,    par_x02,    par_x03,           par_x05,    par_x06,         sum_x09_x10,    par_x09,
+     PHREATIC,             1.0,         par_x35,             0.0,         pow_x11,        0.0,        0.0,               0.0,    par_x12,                 0.0,        0.0,
+      DEEP_GW,             1.0,             0.0,             0.0,             0.0,        0.0,        0.0,               0.0,        0.0,                 0.0,        0.0,
+ #    TOPSOIL,             1.0,           x(28),           x(08),           x(04),      x(02),      x(03),             x(05),      x(06),         x(09)+x(10),      x(09),
+ #   PHREATIC,             1.0,           x(35),             0.0,           x(11),        0.0,        0.0,               0.0,      x(12),                 0.0,        0.0,
+ #    DEEP_GW,             1.0,             0.0,             0.0,             0.0,        0.0,        0.0,               0.0,        0.0,                 0.0,        0.0,
+:EndSoilParameterList
+
+#-----------------------------------------------------------------
+# Land Use Parameters
+#-----------------------------------------------------------------
+:LandUseParameterList
+  :Parameters, MIN_MELT_FACTOR,     MAX_MELT_FACTOR,    DD_MELT_TEMP,  DD_AGGRADATION, REFREEZE_FACTOR, REFREEZE_EXP, DD_REFREEZE_TEMP, HMETS_RUNOFF_COEFF,
+       :Units,          mm/d/C,              mm/d/C,               C,            1/mm,          mm/d/C,            -,                C,                  -,
+    [DEFAULT],         par_x24,         sum_x24_x25,         par_x26,         par_x27,         par_x18,      par_x17,          par_x16,            par_x01,
+#                        x(24),         x(24)+x(25),           x(26),           x(27),           x(18),        x(17),            x(16),              x(01),
+:EndLandUseParameterList
+:LandUseParameterList
+  :Parameters,   GAMMA_SHAPE,     GAMMA_SCALE,    GAMMA_SHAPE2,    GAMMA_SCALE2,    FOREST_SPARSENESS,
+       :Units,             -,               -,               -,               -,                    -,
+    [DEFAULT],       par_x20,         par_x21,         par_x22,         par_x23,                  0.0,
+    #                  x(20),           x(21),           x(22),           x(23),                  0.0,
+:EndLandUseParameterList
+
+#-----------------------------------------------------------------
+# Vegetation Parameters
+#-----------------------------------------------------------------
+:VegetationParameterList
+  :Parameters,  RAIN_ICEPT_PCT,  SNOW_ICEPT_PCT,    SAI_HT_RATIO
+       :Units,               -,               -,               -
+    [DEFAULT],             0.0,             0.0,             0.0
+:EndVegetationParameterList
+
+:SeasonalRelativeLAI
+  FOREST, 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0
+:EndSeasonalRelativeLAI
+:SeasonalRelativeHeight
+  FOREST, 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0
+:EndSeasonalRelativeHeight
---
+++ tests/test_blended.py
@@ -0,0 +1,325 @@
+import datetime as dt
+import tempfile
+
+import numpy as np
+
+from ravenpy.models import (
+    BLENDED,
+    BLENDED_OST,
+    )
+
+from ravenpy.utilities.testdata import get_local_testdata
+
+from .common import _convert_2d
+
+TS = get_local_testdata(
+    "raven-gr4j-cemaneige/Salmon-River-Near-Prince-George_meteo_daily.nc"
+)
+
+class TestBLENDED:
+    def test_simple(self):
+        model = BLENDED()
+        params = (
+            2.930702E-02,  # par_x01
+            2.211166E+00,  # par_x02
+            2.166229E+00,  # par_x03
+            0.0002254976,  # feed 10**par_x04; not par_x4=-3.646858076
+            2.173976E+01,  # par_x05
+            1.565091E+00,  # par_x06
+            6.211146E+00,  # par_x07
+            9.313578E-01,  # par_x08
+            3.486263E-02,  # par_x09
+            0.251835,      # feed par_x09+x10; not par_x10=0.21697237
+            0.0002279250,  # feed 10**par_x11; not par_x11=-3.642208036
+            1.214339E+00,  # par_x12
+            4.736668E-02,  # par_x13
+            0.2070342,     # feed par_x13+x14; not par_x14=0.15966752
+            7.806324E-02,  # par_x15
+            -1.336429E+00, # par_x16
+            2.189741E-01,  # par_x17
+            3.845617E+00,  # par_x18
+            2.950022E-01,  # par_x19
+            4.827523E-01,  # par_x20
+            4.099820E+00,  # par_x21
+            1.283144E+01,  # par_x22
+            5.937894E-01,  # par_x23
+            1.651588E+00,  # par_x24
+            1.705806,      # feed par_x24+x25; not par_x25=0.054218
+            3.719308E-01,  # par_x26
+            7.121015E-02,  # par_x27
+            1.906440E-02,  # par_x28
+            4.080660E-01,  # par_x29
+            9.415693E-01,  # par_x30
+            -1.856108E+00, # par_x31
+            2.356995E+00,  # par_x32
+            1.110496E+00,  # par_x33
+            1.042556E+00,  # par_x34      <<<<<<<<<<< NOT TEMPLATED YET: IS :RainCorrection in RVT
+            7.510967E-03,  # par_x35      <<<<<<<<<<< NOT TEMPLATED YET: IS :SnowCorrection in RVT
+            5.321608E-01,  # par_r01
+            2.891977E-02,  # par_r02
+            9.605330E-01,  # par_r03
+            6.128669E-01,  # par_r04
+            9.558293E-01,  # par_r05
+            1.008196E-01,  # par_r06
+            9.275730E-02,  # par_r07
+            7.469583E-01,  # par_r08
+        )
+
+        model(
+            TS,
+            start_date=dt.datetime(2000, 1, 1),
+            end_date=dt.datetime(2002, 1, 1),
+            area=4250.6,
+            elevation=843.0,
+            latitude=54.4848,
+            longitude=-123.3659,
+            # should actually also contain:
+            #       forest_fraction and slope (for RVH) and
+            #       :RainCorrection and SnowCorrection (default 1.0) (for RVT) but might be a parameter (here: {param.x33} and {param.x34})
+            params=params,
+            suppress_output=True,
+        )
+
+        d = model.diagnostics
+
+        np.testing.assert_almost_equal(d["DIAG_NASH_SUTCLIFFE"], -0.913785, 4)
+
+
+
+
+class TestBLENDED_OST:
+    def test_simple(self):
+        model = BLENDED_OST()
+        params = (
+            2.930702E-02,  # par_x01
+            2.211166E+00,  # par_x02
+            2.166229E+00,  # par_x03
+            0.0002254976,  # feed 10**par_x04; not par_x4=-3.646858076
+            2.173976E+01,  # par_x05
+            1.565091E+00,  # par_x06
+            6.211146E+00,  # par_x07
+            9.313578E-01,  # par_x08
+            3.486263E-02,  # par_x09
+            0.251835,      # feed par_x09+x10; not par_x10=0.21697237
+            0.0002279250,  # feed 10**par_x11; not par_x11=-3.642208036
+            1.214339E+00,  # par_x12
+            4.736668E-02,  # par_x13
+            0.2070342,     # feed par_x13+x14; not par_x14=0.15966752
+            7.806324E-02,  # par_x15
+            -1.336429E+00, # par_x16
+            2.189741E-01,  # par_x17
+            3.845617E+00,  # par_x18
+            2.950022E-01,  # par_x19
+            4.827523E-01,  # par_x20
+            4.099820E+00,  # par_x21
+            1.283144E+01,  # par_x22
+            5.937894E-01,  # par_x23
+            1.651588E+00,  # par_x24
+            1.705806,      # feed par_x24+x25; not par_x25=0.054218
+            3.719308E-01,  # par_x26
+            7.121015E-02,  # par_x27
+            1.906440E-02,  # par_x28
+            4.080660E-01,  # par_x29
+            9.415693E-01,  # par_x30
+            -1.856108E+00, # par_x31
+            2.356995E+00,  # par_x32
+            1.110496E+00,  # par_x33
+            1.042556E+00,  # par_x34      <<<<<<<<<<< NOT TEMPLATED YET: IS :RainCorrection in RVT
+            7.510967E-03,  # par_x35      <<<<<<<<<<< NOT TEMPLATED YET: IS :SnowCorrection in RVT
+            5.321608E-01,  # par_r01
+            2.891977E-02,  # par_r02
+            9.605330E-01,  # par_r03
+            6.128669E-01,  # par_r04
+            9.558293E-01,  # par_r05
+            1.008196E-01,  # par_r06
+            9.275730E-02,  # par_r07
+            7.469583E-01,  # par_r08
+        )
+        low = (
+            0.0,       # par_x16              par_x01
+            0.1,       # par_x22 NEW          par_x02
+            0.5,       # par_x23 NEW          par_x03
+            -5.0,      # par_x18              10**par_x04
+            0.0,       # par_x24 NEW          par_x05
+            0.5,       # par_x25 NEW          par_x06
+            5.0,       # par_x26 NEW          par_x07
+            0.0,       # par_x15              par_x08
+            0.0,       # par_x27 NEW          par_x09
+            0.0,       # par_x28 NEW          par_x09+x10
+            -5.0,      # par_x19              10**par_x11
+            0.5,       # par_x29 NEW          par_x12
+            0.0,       # par_x09              par_x13
+            0.01,      # par_x10              par_x13+x14
+            0.005,     # par_x11              par_x15
+            -5.0,      # par_x12              par_x16
+            0.0,       # par_x14              par_x17
+            0.0,       # par_x13              par_x18
+            0.0,       # par_x30 NEW          par_x19
+            0.3,       # par_x01              par_x20
+            0.01,      # par_x02              par_x21
+            0.5,       # par_x03              par_x22
+            0.15,      # par_x04              par_x23
+            1.5,       # par_x05              par_x24
+            0.0,       # par_x06              par_x24+x25
+            -1.0,      # par_x07              par_x26
+            0.01,      # par_x08              par_x27
+            0.00001,   # par_x17              par_x28
+            0.0,       # par_x20              par_x29
+            0.0,       # par_x21              par_x30
+            -3.0,      # par_x31 NEW          par_x31
+            0.5,       # par_x32 NEW          par_x32
+            0.8,       # par_x33 NEW          par_x33
+            0.8,       # par_x34 NEW          par_x34
+            0.0,       # par_x35 NEW          par_x35
+            0.0,       # par_x36 NEW          par_r01
+            0.0,       # par_x37 NEW          par_r02
+            0.0,       # par_x38 NEW          par_r03
+            0.0,       # par_x39 NEW          par_r04
+            0.0,       # par_x40 NEW          par_r05
+            0.0,       # par_x41 NEW          par_r06
+            0.0,       # par_x42 NEW          par_r07
+            0.0,       # par_x43 NEW          par_r08
+        )
+        high = (
+            1.0,       # par_x16              par_x01
+            3.0,       # par_x22 NEW          par_x02
+            3.0,       # par_x23 NEW          par_x03
+            -1.0,      # par_x18              10**par_x04
+            100.0,     # par_x24 NEW          par_x05
+            2.0,       # par_x25 NEW          par_x06
+            10.0,      # par_x26 NEW          par_x07
+            3.0,       # par_x15              par_x08
+            0.05,      # par_x27 NEW          par_x09
+            0.45,      # par_x28 NEW          par_x09+x10
+            -2.0,      # par_x19              10**par_x11
+            2.0,       # par_x29 NEW          par_x12
+            0.1,       # par_x09              par_x13
+            0.3,       # par_x10              par_x13+x14
+            0.1,       # par_x11              par_x15
+            2.0,       # par_x12              par_x16
+            1.0,       # par_x14              par_x17
+            5.0,       # par_x13              par_x18
+            0.4,       # par_x30 NEW          par_x19
+            20.0,      # par_x01              par_x20
+            5.0,       # par_x02              par_x21
+            13.0,      # par_x03              par_x22
+            1.5,       # par_x04              par_x23
+            3.0,       # par_x05              par_x24
+            5.0,       # par_x06              par_x24+x25
+            1.0,       # par_x07              par_x26
+            0.2,       # par_x08              par_x27
+            0.02,      # par_x17              par_x28
+            0.5,       # par_x20              par_x29
+            2.0,       # par_x21              par_x30
+            3.0,       # par_x31 NEW          par_x31
+            4.0,       # par_x32 NEW          par_x32
+            1.2,       # par_x33 NEW          par_x33
+            1.2,       # par_x34 NEW          par_x34
+            0.02,      # par_x35 NEW          par_x35
+            1.0,       # par_x36 NEW          par_r01
+            1.0,       # par_x37 NEW          par_r02
+            1.0,       # par_x38 NEW          par_r03
+            1.0,       # par_x39 NEW          par_r04
+            1.0,       # par_x40 NEW          par_r05
+            1.0,       # par_x41 NEW          par_r06
+            1.0,       # par_x42 NEW          par_r07
+            1.0,       # par_x43 NEW          par_r08
+        )
+
+        model(
+            TS,
+            start_date=dt.datetime(1954, 1, 1),
+            duration=208,
+            area=4250.6,
+            elevation=843.0,
+            latitude=54.4848,
+            longitude=-123.3659,
+            params=params,
+            lowerBounds=low,
+            upperBounds=high,
+            algorithm="DDS",
+            random_seed=0,
+            max_iterations=10,
+        )
+
+        d = model.diagnostics
+
+        np.testing.assert_almost_equal(d["DIAG_NASH_SUTCLIFFE"], -1.51237, 4)
+
+        opt_para = model.optimized_parameters
+        opt_func = model.obj_func
+
+        expected_value = [
+            2.400718E-02,
+            1.516941E+00,
+            2.759658E+00,
+            -4.075778E+00,
+            3.483657E+01,
+            1.972741E+00,
+            7.476240E+00,
+            2.966089E+00,
+            2.035387E-03,
+            3.379254E-01,
+            -4.579974E+00,
+            6.867875E-01,
+            8.920417E-02,
+            1.681178E-01,
+            8.827804E-02,
+            -1.475022E+00,
+            4.722976E-01,
+            4.528209E+00,
+            2.273521E-01,
+            8.036873E+00,
+            3.461021E+00,
+            6.880423E+00,
+            1.312190E+00,
+            2.752630E+00,
+            1.515566E+00,
+            -1.499868E-01,
+            6.437522E-02,
+            1.013312E-02,
+            1.089699E-01,
+            1.462368E+00,
+            -1.620150E+00,
+            3.619720E+00,
+            1.130258E+00,
+            1.020023E+00,
+            1.622190E-02,
+            7.319023E-02,
+            1.081170E-01,
+            1.222980E-01,
+            4.622038E-01,
+            4.863545E-02,
+            3.800171E-01,
+            9.620678E-01,
+            7.091240E-01
+        ]
+        np.testing.assert_almost_equal(
+            opt_para,
+            expected_value,
+            4,
+            err_msg="calibrated parameter set is not matching expected value",
+        )
+        np.testing.assert_almost_equal(
+            opt_func,
+            1.51237,
+            4,
+            err_msg="calibrated NSE is not matching expected value",
+        )
+
+        blended = BLENDED()
+        blended(
+            TS,
+            start_date=dt.datetime(1954, 1, 1),
+            duration=208,
+            area=4250.6,
+            elevation=843.0,
+            latitude=54.4848,
+            longitude=-123.3659,
+            params=model.calibrated_params,
+            #params=model.optimized_parameters,           # why is that "calibrated_params" for other tests??
+        )
+
+        np.testing.assert_almost_equal(
+            blended.diagnostics["DIAG_NASH_SUTCLIFFE"], d["DIAG_NASH_SUTCLIFFE"], 4
+        )
