203ae1d424d7946438b582ef3daaf249b6a774f2
Julie <juliane.mai@uwaterloo.ca>
2021-03-09 21:22:16 -0500

    Adding new model BLENDED to model suite
    BLENDED model allows for calibration of model parameters and model structure

--- ravenpy/models/emulators.py
+++ ravenpy/models/emulators.py
@@ -12,10 +12,12 @@ __all__ = [
     "MOHYSE",
     "HMETS",
     "HBVEC",
+    "BLENDED",
     "GR4JCN_OST",
     "MOHYSE_OST",
     "HMETS_OST",
     "HBVEC_OST",
+    "BLENDED_OST",
     "get_model",
     "Routing",
 ]
@@ -437,6 +439,192 @@ class HBVEC_OST(Ostrich, HBVEC):
     def derived_parameters(self):
         self._monthly_average()
 
+class BLENDED(GR4JCN):
+    identifier = "blended"
+    templates = tuple((Path(__file__).parent / "raven-blended").glob("*.rv?"))
+
+    params = namedtuple(
+        "BLENDEDParams", ", ".join(["par_x{:02}".format(i) for i in range(1, 36)]+["par_r{:02}".format(i) for i in range(1, 9)])
+    )
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.rvp = RV(params=BLENDED.params(*((None,) * len(BLENDED.params._fields))))
+        self.rvt = RVT(**{k: nc() for k in std_vars})
+        self.rvi = RVI(evaporation="PET_OUDIN", rain_snow_fraction="RAINSNOW_HBV")
+        self.rvc = RVC(soil0=None, soil1=None, basin_state=BasinIndexCommand())
+        self.rvd = RV(
+            TOPSOIL_mm=None,
+            PHREATIC_mm=None,
+            TOPSOIL_hlf=None,
+            PHREATIC_hlf=None,
+            POW_X04=None,
+            POW_X11=None,
+            SUM_X09_X10=None,
+            SUM_X13_X14=None,
+            SUM_X24_X25=None,
+        )
+
+    def derived_parameters(self):
+        self.rvd["TOPSOIL_hlf"] = self.rvp.params.par_x29 * 0.5 * 1000.0
+        self.rvd["PHREATIC_hlf"] = self.rvp.params.par_x30 * 0.5 * 1000.0
+        self.rvd["TOPSOIL_mm"] = self.rvp.params.par_x29 * 1000.0
+        self.rvd["PHREATIC_mm"] = self.rvp.params.par_x30 * 1000.0
+        self.rvd[
+            "SUM_X09_X10"
+        ] = self.rvp.params.par_x10  # + self.rvp.params.par_x09
+        self.rvd[
+            "SUM_X13_X14"
+        ] = self.rvp.params.par_x14  # + self.rvp.params.par_x13
+        self.rvd[
+            "SUM_X24_X25"
+        ] = self.rvp.params.par_x25  # + self.rvp.params.par_x24
+        self.rvd[
+            "POW_X04"
+        ] = self.rvp.params.par_x04  # 10.0**self.rvp.params.par_x04  #
+        self.rvd[
+            "POW_X11"
+        ] = self.rvp.params.par_x11  # 10.0**self.rvp.params.par_x11  #
+
+        # Default initial conditions if none are given
+        if self.rvc.hru_state is None:
+            soil0 = (
+                self.rvd["TOPSOIL_hlf"] if self.rvc.soil0 is None else self.rvc.soil0
+            )
+            soil1 = (
+                self.rvd["PHREATIC_hlf"] if self.rvc.soil1 is None else self.rvc.soil1
+            )
+            self.rvc.hru_state = HRUState(soil0=soil0, soil1=soil1)
+
+
+class BLENDED_OST(Ostrich, BLENDED):
+    _p = Path(__file__).parent / "ostrich-blended"
+    templates = tuple(_p.glob("model/*.rv?")) + tuple(_p.glob("*.t??"))
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.rvi.suppress_output = True
+        self.txt = Ost(
+            algorithm="DDS",
+            max_iterations=50,
+            lowerBounds=BLENDED.params(
+                None,       # par_x16              par_x01
+                None,       # par_x22 NEW          par_x02
+                None,       # par_x23 NEW          par_x03
+                None,       # par_x18              10**par_x04
+                None,       # par_x24 NEW          par_x05
+                None,       # par_x25 NEW          par_x06
+                None,       # par_x26 NEW          par_x07
+                None,       # par_x15              par_x08
+                None,       # par_x27 NEW          par_x09
+                None,       # par_x28 NEW          par_x09+par_x10
+                None,       # par_x19              10**par_x11
+                None,       # par_x29 NEW          par_x12
+                None,       # par_x09              par_x13
+                None,       # par_x10              par_x13+par_x14
+                None,       # par_x11              par_x15
+                None,       # par_x12              par_x16
+                None,       # par_x14              par_x17
+                None,       # par_x13              par_x18
+                None,       # par_x30 NEW          par_x19
+                None,       # par_x01              par_x20
+                None,       # par_x02              par_x21
+                None,       # par_x03              par_x22
+                None,       # par_x04              par_x23
+                None,       # par_x05              par_x24
+                None,       # par_x06              par_x24+par_x25
+                None,       # par_x07              par_x26
+                None,       # par_x08              par_x27
+                None,       # par_x17              par_x28
+                None,       # par_x20              par_x29
+                None,       # par_x21              par_x30
+                None,       # par_x31 NEW          par_x31
+                None,       # par_x32 NEW          par_x32
+                None,       # par_x33 NEW          par_x33
+                None,       # par_x34 NEW          par_x34
+                None,       # par_x35 NEW          par_x35
+                None,       # par_x36 NEW          par_r01
+                None,       # par_x37 NEW          par_r02
+                None,       # par_x38 NEW          par_r03
+                None,       # par_x39 NEW          par_r04
+                None,       # par_x40 NEW          par_r05
+                None,       # par_x41 NEW          par_r06
+                None,       # par_x42 NEW          par_r07
+                None,       # par_x43 NEW          par_r08
+            ),
+            upperBounds=BLENDED.params(
+                None,       # par_x16              par_x01
+                None,       # par_x22 NEW          par_x02
+                None,       # par_x23 NEW          par_x03
+                None,       # par_x18              10**par_x04
+                None,       # par_x24 NEW          par_x05
+                None,       # par_x25 NEW          par_x06
+                None,       # par_x26 NEW          par_x07
+                None,       # par_x15              par_x08
+                None,       # par_x27 NEW          par_x09
+                None,       # par_x28 NEW          par_x09+par_x10
+                None,       # par_x19              10**par_x11
+                None,       # par_x29 NEW          par_x12
+                None,       # par_x09              par_x13
+                None,       # par_x10              par_x13+par_x14
+                None,       # par_x11              par_x15
+                None,       # par_x12              par_x16
+                None,       # par_x14              par_x17
+                None,       # par_x13              par_x18
+                None,       # par_x30 NEW          par_x19
+                None,       # par_x01              par_x20
+                None,       # par_x02              par_x21
+                None,       # par_x03              par_x22
+                None,       # par_x04              par_x23
+                None,       # par_x05              par_x24
+                None,       # par_x06              par_x24+par_x25
+                None,       # par_x07              par_x26
+                None,       # par_x08              par_x27
+                None,       # par_x17              par_x28
+                None,       # par_x20              par_x29
+                None,       # par_x21              par_x30
+                None,       # par_x31 NEW          par_x31
+                None,       # par_x32 NEW          par_x32
+                None,       # par_x33 NEW          par_x33
+                None,       # par_x34 NEW          par_x34
+                None,       # par_x35 NEW          par_x35
+                None,       # par_x36 NEW          par_r01
+                None,       # par_x37 NEW          par_r02
+                None,       # par_x38 NEW          par_r03
+                None,       # par_x39 NEW          par_r04
+                None,       # par_x40 NEW          par_r05
+                None,       # par_x41 NEW          par_r06
+                None,       # par_x42 NEW          par_r07
+                None,       # par_x43 NEW          par_r08
+            ),
+        )
+
+    def derived_parameters(self):
+        """Derived parameters are computed by Ostrich."""
+        pass
+
+    def ost2raven(self, ops):
+        """Return a list of parameter names calibrated by Ostrich that match Raven's parameters.
+
+        Parameters
+        ----------
+        ops: dict
+          Optimal parameter set returned by Ostrich.
+
+        Returns
+        -------
+        BLENDEDParams named tuple
+          Parameters expected by Raven.
+        """
+        names = ["par_x{:02}".format(i) for i in range(1, 36)]+["par_r{:02}".format(i) for i in range(1, 9)]
+        names[3]  = "pow_x04"
+        names[9]  = "sum_x09_x10"
+        names[10] = "pow_x11"
+        names[13] = "sum_x13_x14"
+        names[24] = "sum_x24_x25"
+
+        out = [ops[n] for n in names]
+        return self.params(*out)
 
 class Routing(Raven):
     """Routing model - no hydrological modeling"""
@@ -480,7 +668,7 @@ def get_model(name):
     model_cls = getattr(emulators, name, None)
 
     if model_cls is None:
-        for m in [GR4JCN, MOHYSE, HMETS, HBVEC]:
+        for m in [GR4JCN, MOHYSE, HMETS, HBVEC, BLENDED]:
             if m.identifier == name:
                 model_cls = m
 
