7d820ee3a4b6a3984f453c8a979859a1308d2046
Julie <juliane.mai@uwaterloo.ca>
2021-03-17 09:02:35 -0400

    Unlocking everything in second test. Minor formatting.

--- tests/test_canadianshield.py
+++ tests/test_canadianshield.py
@@ -28,40 +28,40 @@ class TestCANADIANSHIELD:
             CANADIANSHIELD.HRU_BEDROCK(**hru_default_values),
         )
         params = (
-            4.7230430e-01,  # par_x01
-            8.1639220e-01,  # par_x02
-            9.8619760e-02,  # par_x03
-            3.9269990e-03,  # par_x04
-            4.6907360e-02,  # par_x05
-            4.9552840e-01,  # feed par_x05+x06; not par_x06=4.4862100E-01  4.955284E-01
-            6.8034920e00,  # par_x07
-            4.3305020e-03,  # feed 10**par_x08; not par_x8=-2.363462E+00   4.330502E-03
-            1.0142590e-05,  # feed 10**par_x09; not par_x9=-4.993851E+00   1.014259E-05
-            1.8234700e00,  # par_x10
-            5.1221540e-01,  # par_x11
-            9.0175550e00,  # par_x12
-            3.0771030e01,  # par_x13
-            5.0940950e01,  # par_x14
-            1.6942270e-01,  # par_x15
-            8.2341220e-02,  # par_x16
-            2.3459530e-01,  # feed par_x16+x17; not par_x17=1.5225400E-01  2.345953E-01
-            7.3090400e-02,  # par_x18
-            1.2840520e00,  # par_x19
-            3.6534150e00,  # par_x20
-            2.3065150e01,  # par_x21
-            2.4021830e00,  # par_x22
-            2.5220950e00,  # par_x23
-            5.8034490e-01,  # par_x24
-            1.6141570e00,  # par_x25
-            6.0317810e00,  # par_x26
-            3.1112980e-01,  # par_x27
-            6.7169510e-02,  # par_x28
-            5.8375950e-05,  # par_x29
-            9.8247230e00,  # par_x30
-            9.0074760e-01,  # par_x31
-            8.0405730e-01,  # par_x32
-            1.1790030e00,  # par_x33
-            7.9800130e-01,  # par_x34
+            4.72304300e-01,  # par_x01
+            8.16392200e-01,  # par_x02
+            9.86197600e-02,  # par_x03
+            3.92699900e-03,  # par_x04
+            4.69073600e-02,  # par_x05
+            4.95528400e-01,  # feed par_x05+x06; not par_x06=4.4862100E-01
+            6.803492000e00,  # par_x07
+            4.33050200e-03,  # feed 10**par_x08; not par_x8=-2.363462E+00
+            1.01425900e-05,  # feed 10**par_x09; not par_x9=-4.993851E+00
+            1.823470000e00,  # par_x10
+            5.12215400e-01,  # par_x11
+            9.017555000e00,  # par_x12
+            3.077103000e01,  # par_x13
+            5.094095000e01,  # par_x14
+            1.69422700e-01,  # par_x15
+            8.23412200e-02,  # par_x16
+            2.34595300e-01,  # feed par_x16+x17; not par_x17=1.5225400E-01
+            7.30904000e-02,  # par_x18
+            1.284052000e00,  # par_x19
+            3.653415000e00,  # par_x20
+            2.306515000e01,  # par_x21
+            2.402183000e00,  # par_x22
+            2.522095000e00,  # par_x23
+            5.80344900e-01,  # par_x24
+            1.614157000e00,  # par_x25
+            6.031781000e00,  # par_x26
+            3.11129800e-01,  # par_x27
+            6.71695100e-02,  # par_x28
+            5.83759500e-05,  # par_x29
+            9.824723000e00,  # par_x30
+            9.00747600e-01,  # par_x31
+            8.04057300e-01,  # par_x32
+            1.179003000e00,  # par_x33
+            7.98001300e-01,  # par_x34
         )
 
         model(
@@ -87,40 +87,40 @@ class TestCANADIANSHIELD_OST:
             CANADIANSHIELD.HRU_BEDROCK(**hru_default_values),
         )
         params = (
-            4.7230430e-01,  # par_x01
-            8.1639220e-01,  # par_x02
-            9.8619760e-02,  # par_x03
-            3.9269990e-03,  # par_x04
-            4.6907360e-02,  # par_x05
-            4.9552840e-01,  # feed par_x05+x06; not par_x06=4.4862100E-01
-            6.8034920e00,  # par_x07
-            4.3305020e-03,  # feed 10**par_x08; not par_x8=-2.363462E+00
-            1.0142590e-05,  # feed 10**par_x09; not par_x9=-4.993851E+00
-            1.8234700e00,  # par_x10
-            5.1221540e-01,  # par_x11
-            9.0175550e00,  # par_x12
-            3.0771030e01,  # par_x13
-            5.0940950e01,  # par_x14
-            1.6942270e-01,  # par_x15
-            8.2341220e-02,  # par_x16
-            2.3459530e-01,  # feed par_x16+x17; not par_x17=1.5225400E-01
-            7.3090400e-02,  # par_x18
-            1.2840520e00,  # par_x19
-            3.6534150e00,  # par_x20
-            2.3065150e01,  # par_x21
-            2.4021830e00,  # par_x22
-            2.5220950e00,  # par_x23
-            5.8034490e-01,  # par_x24
-            1.6141570e00,  # par_x25
-            6.0317810e00,  # par_x26
-            3.1112980e-01,  # par_x27
-            6.7169510e-02,  # par_x28
-            5.8375950e-05,  # par_x29
-            9.8247230e00,  # par_x30
-            9.0074760e-01,  # par_x31
-            8.0405730e-01,  # par_x32
-            1.1790030e00,  # par_x33
-            7.9800130e-01,  # par_x34
+            4.72304300e-01,  # par_x01
+            8.16392200e-01,  # par_x02
+            9.86197600e-02,  # par_x03
+            3.92699900e-03,  # par_x04
+            4.69073600e-02,  # par_x05
+            4.95528400e-01,  # feed par_x05+x06; not par_x06=4.4862100E-01
+            6.803492000e00,  # par_x07
+            4.33050200e-03,  # feed 10**par_x08; not par_x8=-2.363462E+00
+            1.01425900e-05,  # feed 10**par_x09; not par_x9=-4.993851E+00
+            1.823470000e00,  # par_x10
+            5.12215400e-01,  # par_x11
+            9.017555000e00,  # par_x12
+            3.077103000e01,  # par_x13
+            5.094095000e01,  # par_x14
+            1.69422700e-01,  # par_x15
+            8.23412200e-02,  # par_x16
+            2.34595300e-01,  # feed par_x16+x17; not par_x17=1.5225400E-01
+            7.30904000e-02,  # par_x18
+            1.284052000e00,  # par_x19
+            3.653415000e00,  # par_x20
+            2.306515000e01,  # par_x21
+            2.402183000e00,  # par_x22
+            2.522095000e00,  # par_x23
+            5.80344900e-01,  # par_x24
+            1.614157000e00,  # par_x25
+            6.031781000e00,  # par_x26
+            3.11129800e-01,  # par_x27
+            6.71695100e-02,  # par_x28
+            5.83759500e-05,  # par_x29
+            9.824723000e00,  # par_x30
+            9.00747600e-01,  # par_x31
+            8.04057300e-01,  # par_x32
+            1.179003000e00,  # par_x33
+            7.98001300e-01,  # par_x34
         )
         low = (
             0.010,  # par_x01
@@ -213,78 +213,75 @@ class TestCANADIANSHIELD_OST:
 
         np.testing.assert_almost_equal(d["DIAG_NASH_SUTCLIFFE"], -4.790550e-01, 4)
 
+        opt_para = model.optimized_parameters
+        opt_func = model.obj_func
 
-#         opt_para = model.optimized_parameters
-#         opt_func = model.obj_func
+        expected_value = [
+            3.1462940e-01,
+            1.46843200e00,
+            3.1906490e-01,
+            1.25061900e00,
+            3.6118660e-02,
+            4.2020140e-01,
+            6.55302700e00,
+            -1.7296660e00,
+            -4.5009800e00,
+            1.63844700e00,
+            1.87916000e00,
+            3.54916500e00,
+            4.88600200e01,
+            9.03863300e01,
+            9.2422170e-02,
+            2.0133650e-02,
+            2.9473000e-01,
+            5.9477830e-02,
+            1.27923400e00,
+            5.6596230e-01,
+            3.76718000e02,
+            7.0004300e-01,
+            1.46996300e00,
+            7.4085920e-01,
+            5.75679100e00,
+            1.49193800e01,
+            7.5535240e-01,
+            9.4459520e-02,
+            1.8634570e-01,
+            6.6940050e-01,
+            3.92734700e00,
+            1.00278400e00,
+            1.00417400e00,
+            7.8267860e-01,
+        ]
+        np.testing.assert_almost_equal(
+            opt_para,
+            expected_value,
+            4,
+            err_msg="calibrated parameter set is not matching expected value",
+        )
+        np.testing.assert_almost_equal(
+            opt_func,
+            4.790550e-01,
+            4,
+            err_msg="calibrated NSE is not matching expected value",
+        )
 
-#         expected_value = [
-#             2.400718E-02,
-#             1.516941E+00,
-#             2.759658E+00,
-#             -4.075778E+00,
-#             3.483657E+01,
-#             1.972741E+00,
-#             7.476240E+00,
-#             2.966089E+00,
-#             2.035387E-03,
-#             3.379254E-01,
-#             -4.579974E+00,
-#             6.867875E-01,
-#             8.920417E-02,
-#             1.681178E-01,
-#             8.827804E-02,
-#             -1.475022E+00,
-#             4.722976E-01,
-#             4.528209E+00,
-#             2.273521E-01,
-#             8.036873E+00,
-#             3.461021E+00,
-#             6.880423E+00,
-#             1.312190E+00,
-#             2.752630E+00,
-#             1.515566E+00,
-#             -1.499868E-01,
-#             6.437522E-02,
-#             1.013312E-02,
-#             1.089699E-01,
-#             1.462368E+00,
-#             -1.620150E+00,
-#             3.619720E+00,
-#             1.130258E+00,
-#             1.020023E+00,
-#             1.622190E-02,
-#             7.319023E-02,
-#             1.081170E-01,
-#             1.222980E-01,
-#             4.622038E-01,
-#             4.863545E-02,
-#             3.800171E-01,
-#             9.620678E-01,
-#             7.091240E-01
-#         ]
-#         np.testing.assert_almost_equal(
-#             opt_para,
-#             expected_value,
-#             4,
-#             err_msg="calibrated parameter set is not matching expected value",
-#         )
-#         np.testing.assert_almost_equal(
-#             opt_func,
-#             1.47169,
-#             4,
-#             err_msg="calibrated NSE is not matching expected value",
-#         )
+        canadianshield = CANADIANSHIELD()
+        canadianshield.rvh.hrus = (
+            CANADIANSHIELD.HRU_ORGANIC(**hru_default_values),
+            CANADIANSHIELD.HRU_BEDROCK(**hru_default_values),
+        )
+        canadianshield(
+            TS,
+            start_date=dt.datetime(2000, 1, 1),
+            end_date=dt.datetime(2002, 1, 1),
+            # hrus=(hru,),
+            land_use_classes=(lu,),
+            params=model.calibrated_params,
+            suppress_output=True,
+        )
 
-#         canadianshield = CANADIANSHIELD()
-#         canadianshield(
-#             TS,
-#             start_date=dt.datetime(1954, 1, 1),
-#             duration=208,
-#             hrus=(hru,),
-#             land_use_classes=(lu,),
-#             params=model.calibrated_params,
-#         )
-
-#         np.testing.assert_almost_equal(
-#             canadianshield.diagnostics["DIAG_NASH_SUTCLIFFE"], d["DIAG_NASH_SUTCLIFFE"], 4
-#         )
+        np.testing.assert_almost_equal(
+            canadianshield.diagnostics["DIAG_NASH_SUTCLIFFE"],
+            d["DIAG_NASH_SUTCLIFFE"],
+            4,
+        )
