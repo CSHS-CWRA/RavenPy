4904651db78691ac3e9b3329661346d063f0d4c6
Christian Jauvin <cjauvin@gmail.com>
2021-04-06 10:48:16 -0400

    Release 0.3.1

--- setup.py
+++ setup.py
@@ -234,7 +234,7 @@ setup(
         gis=gis_requirements,
     ),
     url="https://github.com/CSHS-CWRA/ravenpy",
-    version="0.3.0",
+    version="0.3.1",
     zip_safe=False,
     cmdclass={
         "install": create_external_deps_install_class(install),
