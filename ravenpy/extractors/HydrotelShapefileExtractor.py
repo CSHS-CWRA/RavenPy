fe0bacb12de375fbda2c90e145193809b8fbacee
Bijan55699 <Bizhanimanzar.mohammad@ouranos.ca>
2022-04-14 15:23:34 -0400

    Soil profile data extracted from Hydrotel shapefile

---
+++ ravenpy/extractors/HydrotelShapefileExtractor.py
@@ -0,0 +1,43 @@
+import warnings
+from collections import defaultdict
+from pathlib import Path
+from typing import List
+
+import geopandas
+
+from ravenpy.config.commands import SoilProfilesCommand
+
+
+class HydrotelShapefileExtractor:
+
+    def __init__(
+        self,
+        shapefile_path,
+        routingproductoutput
+    ):
+        self.routingproductoutput = routingproductoutput
+        if isinstance(shapefile_path, (Path, str)):
+            if Path(shapefile_path).suffix == ".zip":
+                shapefile_path = f"zip://{shapefile_path}"
+            self._df = geopandas.read_file(shapefile_path)
+        elif isinstance(shapefile_path, geopandas.GeoDataFrame):
+            self._df = shapefile_path
+
+    def extract2(self,routingproductoutput) -> SoilProfilesCommand.Record:
+        # This method extracts the soil profile data (layer name, thickness) from the shapefile.
+
+        soil_recs = set()
+        for _, row in self._df.iterrows():
+            soil_recs.add(self._extract_soillayer_thickness(row)) #here we find the unique rows
+        soils = list(soil_recs)
+        soilinfo = []
+        keyss = ['soilprofile']
+        for soil in soils:
+            soilinfo.append(SoilProfilesCommand.Record(soil[0],['TOPSOIL','FAST_RES','SLOW_RES'],soil[2:]))
+        ss = dict.fromkeys(keyss,soilinfo)
+        return (routingproductoutput.update(ss))
+
+    def _extract_soillayer_thickness(self,row):
+
+        items = ["TOPSOIL", "Nhorizons", "th1", "th2", "th3"]  # all 3 layers have the same soil type (Hydrotel configurations)
+        return tuple(row[item] for item in items)
---
+++ ravenpy/extractors/Hydrotel_shapefile.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import geopandas
+
+from ravenpy.config.commands import SoilProfilesCommand
+from ravenpy.extractors.HydrotelShapefileExtractor import HydrotelShapefileExtractor
+from ravenpy.extractors.routing_product import RoutingProductShapefileExtractor
+
+pth = '/home/mohammad/Dossier_travail/Hydrotel/DEH/MG24HA/SLSO_MG24HA_2020/physitel/HRU/raven-testdata/famine/hru_Famine_final.zip'
+rvh_extractor = RoutingProductShapefileExtractor(
+    pth,
+    hru_aspect_convention="ArcGIS",
+    routing_product_version="2.1",
+)
+
+rvh_config = rvh_extractor.extract()
+
+
+rvh_plus_soil = HydrotelShapefileExtractor(pth, rvh_config)
+rvh_plus_soil.extract2(rvh_config)
+print(rvh_plus_soil)
