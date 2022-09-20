f83de89939bc4f8ced887e52466b755e96767c12
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-09-20 12:03:41 -0400

    pin only in tox

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
     "dask",
     "haversine",
     "matplotlib",
-    "netCDF4",  # Pin needed due to issue with wheel (see: https://github.com/Unidata/netcdf4-python/issues/1192)
+    "netCDF4",
     "numpy",
     "pandas",
     "pydantic",
