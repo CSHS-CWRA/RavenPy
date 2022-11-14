050c4a0820d0279ed584c089a88bce4e0853e511
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-11-14 16:19:08 -0500

    fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,6 @@ extensions = [
     "sphinx.ext.viewcode",
     "nbsphinx",
     "IPython.sphinxext.ipython_console_highlighting",
-    "autodoc_indicator",
     "sphinx_click",
     "sphinx_codeautolink",
     "sphinx_copybutton",
