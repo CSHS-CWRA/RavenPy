016898f81924d646464d24f15495ba73c090e923
David Huard <huard.david@ouranos.ca>
2022-11-15 17:23:32 -0500

    Fix zip warning about multiple files with the same name

--- ravenpy/models/__init__.py
+++ ravenpy/models/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-from .base import Ostrich, Raven, RavenError, get_average_annual_runoff
+from .base import Ostrich, Raven, RavenError, RavenWarning, get_average_annual_runoff
 from .emulators import *
 from .multimodel import RavenMultiModel
