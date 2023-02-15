2480f8378288c9548ab4b04cc3ac5878a536f576
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-02-15 15:14:43 -0500

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -290,7 +290,7 @@ setup(
         gis=gis_requirements,
     ),
     url="https://github.com/CSHS-CWRA/ravenpy",
-    version="0.10.0",
+    version="0.11.0",
     zip_safe=False,
     cmdclass={
         "install": create_external_deps_install_class(install),
