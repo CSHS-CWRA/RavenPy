63dcaf4268cb4eacb40ca012701b70d79d30368f
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-04-17 15:14:25 -0400

    fix Pathlib call

--- ravenpy/utilities/forecasting.py
+++ ravenpy/utilities/forecasting.py
@@ -306,8 +306,7 @@ def _shift_esp_time(nc, year, dim="member"):
     )
 
     # Write to disk
-    # FIXME: pathlib.Path().with_stem() was introduced in Python3.9
-    fn = nc.with_stem(nc.stem + "_shifted")
+    fn = nc.parent.joinpath(f"{nc.stem}_shifted{nc.suffix}")
     out.to_netcdf(fn, mode="w")
     return fn
 
