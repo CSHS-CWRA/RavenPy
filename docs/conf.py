0a128600b72604d002afe52bacde77b5b2682ae6
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-05-29 12:45:07 -0400

    notebook cleanup, config changes for running things locally

--- docs/conf.py
+++ docs/conf.py
@@ -154,7 +154,10 @@ pygments_style = "sphinx"
 todo_include_todos = False
 
 # Suppress "WARNING: unknown mimetype for ..." when building EPUB.
-suppress_warnings = ["epub.unknown_project_files", "mystnb.unknown_mime_type"]
+suppress_warnings = [
+    "epub.unknown_project_files",
+    "mystnb.unknown_mime_type"
+]
 
 # -- Options for HTML output -------------------------------------------
 
