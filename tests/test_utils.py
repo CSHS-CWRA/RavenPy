d279570c0e40dbd64a2a9a7c27f7e1317510745c
David Huard <huard.david@ouranos.ca>
2023-11-02 10:21:51 -0400

    handle scalar elevation values in nc_specs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,21 @@
 import pytest
+import xarray as xr
 
 from ravenpy.config.utils import nc_specs
 
 
-def test_nc_specs(get_local_testdata):
+def test_nc_specs(get_local_testdata, tmp_path):
     f = get_local_testdata(
         "raven-gr4j-cemaneige/Salmon-River-Near-Prince-George_meteo_daily.nc"
     )
     attrs = nc_specs(f, "PRECIP", station_idx=1, alt_names=("rain",))
     assert "file_name_nc" in attrs
+
+
+def test_nc_specs_bad(bad_netcdf):
+    # Test with scalar elevation. Should normally have a station dimension, but not always the case.
+    attrs_s = nc_specs(bad_netcdf, "PRECIP", station_idx=1, alt_names=("rain",))
+    assert attrs_s["elevation"] == 1.0
 
 
 @pytest.mark.online
