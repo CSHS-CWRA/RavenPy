7822d30f5d6b4d4c336c64cd8cf69a34f74670b2
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2024-01-12 10:46:11 -0500

    re-enable autodoc-pydantic

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ extensions = [
     "sphinx_click",
     "sphinx_codeautolink",
     "sphinx_copybutton",
-    # "sphinxcontrib.autodoc_pydantic",
+    "sphinxcontrib.autodoc_pydantic",
 ]
 
 linkcheck_ignore = [
