c8dd882271bda90b8ae8c05e53fd5b142a0ca393
David Huard <huard.david@ouranos.ca>
2025-05-07 10:54:10 -0400

    Set default station_idx to None for ReadFromNetCDF

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,6 +68,7 @@ def populate_testing_data(
             "eccc_forecasts/geps_watershed.nc",
             "cmip5/nasa_nex-gddp-1.0_day_inmcm4_historical+rcp45_nex-gddp_1971-1972_subset.nc",
             "cmip5/nasa_nex-gddp-1.0_day_inmcm4_historical+rcp85_nex-gddp_2070-2071_subset.nc",
+            "cmip5/tas_Amon_CanESM2_rcp85_r1i1p1_200601-210012_subset.nc",
             "famine/famine_input.nc",
             "gr4j_cemaneige/solution.rvc",
             "hydro_simulations/raven-gr4j-cemaneige-sim_hmets-0_Hydrographs.nc",
