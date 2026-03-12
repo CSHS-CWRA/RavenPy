f537b8dca478975013c98019249b81b7a633dd18
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2026-03-12 17:09:51 -0400

    fully skip xsdba test
    Signed-off-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- tests/test_bias_correction.py
+++ tests/test_bias_correction.py
@@ -1,11 +1,12 @@
 import pytest
-import xsdba
 from xarray.coding.calendar_ops import convert_calendar
 
 
 # FIXME: This doesn't test Raven functionality; Should we move it to xclim?
 @pytest.mark.skip(reason="This test is not testing Raven functionality")
 class TestBiasCorrect:
+    xsdba = pytest.importorskip("xsdba")
+
     def test_bias_correction(self, open_dataset):
         ds_fut_sub = open_dataset("cmip5/nasa_nex-gddp-1.0_day_inmcm4_historical+rcp85_nex-gddp_2070-2071_subset.nc")
 
@@ -15,9 +16,9 @@ class TestBiasCorrect:
         ds_his_sub = open_dataset("nrcan/NRCAN_1971-1972_subset.nc")
 
         ds_his_sub = convert_calendar(ds_his_sub, "noleap")
-        group = xsdba.Grouper("time.month")
+        group = self.xsdba.Grouper("time.month")
         # Train the model to find the correction factors
-        Adj = xsdba.DetrendedQuantileMapping.train(
+        Adj = self.xsdba.DetrendedQuantileMapping.train(
             ref=ds_ref_sub["pr"],
             hist=ds_his_sub["pr"],
             nquantiles=50,
@@ -29,7 +30,7 @@ class TestBiasCorrect:
         Adj.adjust(ds_fut_sub["pr"], interp="linear")
 
         # Repeat for temperature max
-        Adj = xsdba.DetrendedQuantileMapping.train(
+        Adj = self.xsdba.DetrendedQuantileMapping.train(
             ref=ds_ref_sub["tasmax"],
             hist=ds_his_sub["tasmax"],
             nquantiles=50,
@@ -41,7 +42,7 @@ class TestBiasCorrect:
         Adj.adjust(ds_fut_sub["tasmax"], interp="linear")
 
         # Repeat for tasmin
-        Adj = xsdba.DetrendedQuantileMapping.train(
+        Adj = self.xsdba.DetrendedQuantileMapping.train(
             ref=ds_ref_sub["tasmin"],
             hist=ds_his_sub["tasmin"],
             nquantiles=50,
